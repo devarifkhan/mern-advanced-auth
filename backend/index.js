75b67cdc9cde3b83263bab558e4e383fd78a8f5a
devarifkhan <arifcse209@gmail.com>
2026-02-23 15:11:42 +0600

    refactor: update CORS configuration and API URL handling

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import path from "path";
 
 import { connectDB } from "./db/connectDB.js";
 
@@ -12,22 +11,18 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+const allowedOrigins = [
+	"http://localhost:5173",
+	"https://mern-advanced-auth-sigma.vercel.app",
+];
+
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 app.use(express.json()); // allows us to parse incoming requests:req.body
 app.use(cookieParser()); // allows us to parse incoming cookies
 
 app.use("/api/auth", authRoutes);
-
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
 
 app.listen(PORT, () => {
 	connectDB();
